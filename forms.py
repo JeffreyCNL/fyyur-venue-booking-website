51cb87abae29d37bc8d31792bd54e16cbcdceae7
PostgreSQL administrator <postgres@LAPTOP-2ACOS295.localdomain>
2020-08-04 20:22:48 -0700

    form for default page

--- forms.py
+++ forms.py
@@ -92,7 +92,6 @@ class VenueForm(Form):
         'image_link'
     )
     genres = SelectMultipleField(
-        # TODO implement enum restriction
         'genres', validators=[DataRequired()],
         choices=[
             ('Alternative', 'Alternative'),
@@ -190,14 +189,12 @@ class ArtistForm(Form):
         ]
     )
     phone = StringField(
-        # TODO implement validation logic for state
         'phone'
     )
     image_link = StringField(
         'image_link'
     )
     genres = SelectMultipleField(
-        # TODO implement enum restriction
         'genres', validators=[DataRequired()],
         choices=[
             ('Alternative', 'Alternative'),
@@ -225,7 +222,6 @@ class ArtistForm(Form):
         'website', validators=[URL()]
     )
     facebook_link = StringField(
-        # TODO implement enum restriction
         'facebook_link', validators=[URL()]
     )
     seeking_venue = BooleanField(
@@ -234,4 +230,3 @@ class ArtistForm(Form):
     seeking_description = StringField(
         'seeking_description'
     )
-# TODO IMPLEMENT NEW ARTIST FORM AND NEW SHOW FORM
