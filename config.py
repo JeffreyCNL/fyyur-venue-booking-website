7e35cbd90d4eadde014c0238ef8c02226cceeae8
PostgreSQL administrator <postgres@LAPTOP-2ACOS295.localdomain>
2020-08-04 20:31:02 -0700

    database path change to heroku

--- config.py
+++ config.py
@@ -7,4 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = 'postgres:///fyyur'
+# SQLALCHEMY_DATABASE_URI = 'postgres:///fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgres://uaeumabjvgfwmp:eb5aaf33b22f793a2e2c3d9407559b36d2dfda727ca07f7ee13bbfe29b0f7199@ec2-34-237-89-96.compute-1.amazonaws.com:5432/devk88beeoc2kt'
+
